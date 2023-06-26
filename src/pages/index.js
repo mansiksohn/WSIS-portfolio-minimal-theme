282d26cba2f519f9b6d2b75e02908fd3153a0ebe
Mansik Sohn <mansiksohn@gmail.com>
2023-06-27 00:00:47 +0900

    6/26

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,14 +13,14 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="WSIS" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
         <AboutSection sectionId="about" heading="About Portfolio Minimal" />
         <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <ProjectsSection sectionId="products" heading="Products" />
+        <ContactSection sectionId="miscellaneous" heading="Miscellaneous" />
       </Page>
     </>
   );
